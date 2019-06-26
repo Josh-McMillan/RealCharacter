edfad3c64b87d1072671f4ecb60f4317a7731f62
Joshua McMillan <jmcmillan.programming@gmail.com>
2019-06-25 19:58:47 -0500

    Multi-Display
    Multi-Display works in both editor and builds.
    
    Camera 1 renders GL.wireframe and future puppeter monitoring
    
    Camera 2 renders the actual animation ouput

---
+++ RealCharacter/Assets/Scripts/Display/DisplaySetup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplaySetup : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Connected Displays: " + Display.displays.Length);
+
+        // Display.displays[0] is primary, always on!
+        // Check and activate for render output:
+
+        if (Display.displays.Length > 1)
+        {
+            Display.displays[1].Activate();
+        }
+    }
+}
