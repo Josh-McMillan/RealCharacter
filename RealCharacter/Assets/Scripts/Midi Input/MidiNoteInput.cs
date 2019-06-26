8941ccf639520f3c5d37bfe97aac5048436fe6ed
Joshua McMillan <jmcmillan.programming@gmail.com>
2019-06-26 15:18:49 -0500

    Added Midi and Realtime Animation
    Added support for MIDI note input and MIDI CC input
    Added Realtime Animator for manipulating Transforms based on input sources
    Added rotating cube to SampleScene

---
+++ RealCharacter/Assets/Scripts/Midi Input/MidiNoteInput.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public enum MidiNotes
+{
+    C = 0,
+    Cs = 1,
+    D = 2,
+    Ds = 3,
+    E = 4,
+    F = 5,
+    Fs = 6,
+    G = 7,
+    Gs = 8,
+    A = 9,
+    As = 10,
+    B = 11
+}
+
+public enum NoteInputType
+{
+    Pressed,
+    Held,
+    Released
+}
+
+public class MidiNoteInput : BaseInput
+{
+    [SerializeField] private MidiChannel midiChannel = MidiChannel.All;
+
+    [SerializeField] private MidiNotes midiNote = MidiNotes.C;
+
+    [SerializeField] private int ocatave = 4;
+
+    [SerializeField] private NoteInputType inputType = NoteInputType.Pressed;
+
+    [SerializeField] private float positiveValue = 1.0f;
+
+    [SerializeField] private float negativeValue = 0.0f;
+
+    public override float GetInput()
+    {
+        switch (inputType)
+        {
+            case NoteInputType.Pressed:
+                return MidiMaster.GetKeyDown(midiChannel, (int)midiNote + ocatave + 12) ? positiveValue : negativeValue;
+
+            case NoteInputType.Held:
+                return MidiMaster.GetKey(midiChannel, (int)midiNote + ocatave + 12);
+
+            case NoteInputType.Released:
+                return MidiMaster.GetKeyUp(midiChannel, (int)midiNote + ocatave + 12) ? positiveValue : negativeValue;
+        }
+
+        return 0.0f;
+    }
+}
---
+++ RealCharacter/Assets/Scripts/RealtimeAnimator.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnimationType
+{
+    Translate,
+    Rotate,
+    Scale
+}
+
+public enum Axis
+{
+    X,
+    Y,
+    Z
+}
+
+public class RealtimeAnimator : MonoBehaviour
+{
+    public BaseInput inputSource;
+
+    public AnimationType animationType = AnimationType.Translate;
+
+    public Axis axis = Axis.X;
+
+    public AnimationCurve animationControl;
+
+    public bool doSmoothing = false;
+
+    public float smoothSpeed = 0.15f;
+
+    private float inputValue = 0.0f;
+
+    private float driverValue;
+
+    private float smoothingVelocity = 0.0f;
+
+    private Vector3 drivingVector = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        inputValue = inputSource.GetInput();
+
+        switch (animationType)
+        {
+            case AnimationType.Translate:
+
+                drivingVector = SetTranslationalDriverVector();
+
+                transform.localPosition = drivingVector;
+
+                break;
+
+            case AnimationType.Rotate:
+
+                drivingVector = SetRotationalDriverVector();
+
+                transform.localRotation = Quaternion.Euler(drivingVector);
+                break;
+
+            case AnimationType.Scale:
+
+                drivingVector = SetScalingDriverVector();
+
+                transform.localScale = drivingVector;
+                break;
+        }
+    }
+
+    private void PerformAnimation()
+    {
+    }
+
+    private float GetInputIfAvailable()
+    {
+        if (inputSource != null)
+        {
+            return inputSource.GetInput();
+        }
+
+        Debug.LogWarning("WARNING: " + gameObject.name + "\'s RealtimeAnimator is missing an input source!");
+        return 0.0f;
+    }
+
+    private float SetDriverValue(bool doRotation)
+    {
+        if (doSmoothing)
+        {
+            if (doRotation)
+            {
+                return Mathf.SmoothDampAngle(driverValue, inputValue, ref smoothingVelocity, smoothSpeed);
+            }
+
+            return Mathf.SmoothDamp(driverValue, inputValue, ref smoothingVelocity, smoothSpeed);
+        }
+
+        return inputValue;
+    }
+
+    private Vector3 SetTranslationalDriverVector()
+    {
+        driverValue = SetDriverValue(false);
+
+        switch (axis)
+        {
+            case Axis.X:
+                return new Vector3(animationControl.Evaluate(driverValue), transform.position.y, transform.position.z);
+
+            case Axis.Y:
+                return new Vector3(transform.position.x, animationControl.Evaluate(driverValue), transform.position.z);
+
+            case Axis.Z:
+                return new Vector3(transform.position.x, transform.position.y, animationControl.Evaluate(driverValue));
+        }
+
+        Debug.LogWarning("WARNING: " + gameObject.name + "\'s RealtimeAnimator could not set driver vector!");
+        return Vector3.zero;
+    }
+
+    private Vector3 SetRotationalDriverVector()
+    {
+        driverValue = SetDriverValue(true);
+
+        switch (axis)
+        {
+            case Axis.X:
+                return new Vector3(animationControl.Evaluate(driverValue), 0.0f, 0.0f);
+
+            case Axis.Y:
+                return new Vector3(0.0f, animationControl.Evaluate(driverValue), 0.0f);
+
+            case Axis.Z:
+                return new Vector3(0.0f, 0.0f, animationControl.Evaluate(driverValue));
+        }
+
+        Debug.LogWarning("WARNING: " + gameObject.name + "\'s RealtimeAnimator could not set driver vector!");
+        return Vector3.zero;
+    }
+
+    private Vector3 SetScalingDriverVector()
+    {
+        driverValue = SetDriverValue(false);
+
+        switch (axis)
+        {
+            case Axis.X:
+                return new Vector3(animationControl.Evaluate(driverValue), transform.localScale.y, transform.localScale.z);
+
+            case Axis.Y:
+                return new Vector3(transform.localScale.x, animationControl.Evaluate(driverValue), transform.localScale.z);
+
+            case Axis.Z:
+                return new Vector3(transform.localScale.x, transform.localScale.y, animationControl.Evaluate(driverValue));
+        }
+
+        Debug.LogWarning("WARNING: " + gameObject.name + "\'s RealtimeAnimator could not set driver vector!");
+        return Vector3.zero;
+    }
+}
